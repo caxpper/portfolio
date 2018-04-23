e54f3a529268fb574a949178405c5bcfb16618a5
Alejandro Gaspar <caxpper@hotmail.com>
2018-04-23 12:27:08 -0700

    add photo, delete things I'm not going to use

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@
 
         $('#contact-form').on('submit', function (e) {
             if (!e.isDefaultPrevented()) {
-                var url = "contact_form/contact_form.php";
+                var url = "php_mailer/mail_handler.php";
 
                 $.ajax({
                     type: "POST",
